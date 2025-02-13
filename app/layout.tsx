46f601514a7a9249208c069e579ffba7dae4d7fc
joneseugene <joneseugeneceo@gmail.com>
2025-02-13 20:38:58 +0000

    Init

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,22 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import localFont from "next/font/local";
 import { ReactNode } from "react";
+import { Inter } from "next/font/google";
 
 
-const ibmPlexSans = localFont({
-  src: [
-    { path: "/fonts/IBMPlexSans-Regular.ttf", weight: "400", style: "normal" },
-    { path: "/fonts/IBMPlexSans-Medium.ttf", weight: "500", style: "normal" },
-    { path: "/fonts/IBMPlexSans-SemiBold.ttf", weight: "600", style: "normal" },
-    { path: "/fonts/IBMPlexSans-Bold.ttf", weight: "700", style: "normal" },
-  ],
-});
-
-const bebasNeue = localFont({
-  src: [
-    { path: "/fonts/BebasNeue-Regular.ttf", weight: "400", style: "normal" },
-  ],
-  variable: "--bebas-neue",
-});
+const inter = Inter({ subsets: ["latin"]})
 
 export const metadata: Metadata = {
-  title: "BookWise",
+  title: "Youtube",
   description:
-    "BookWise is a book borrowing university library management solution.",
+    "A clone of youtube website",
 };
 
 const RootLayout = async ({ children }: { children: ReactNode }) => {
 
   return (
     <html lang="en">
-        <body className={`${ibmPlexSans.className} ${bebasNeue.variable} antialiased`}>
+        <body className={inter.className}>
           {children}
         </body>
     </html>
--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-      {
-        protocol: "https",
-        hostname: "m.media-amazon.com",
-      }
-    ]
-  }
+  
 };
 
 export default nextConfig;
